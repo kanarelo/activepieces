768dd1ec702ffd853540f91d16d2a1961040e65b
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-03-09 12:54:26 +0300

    chore: creates types out of json

--- packages/pieces/openapi/src/lib/common/create-actions.ts
+++ packages/pieces/openapi/src/lib/common/create-actions.ts
@@ -1,21 +1,31 @@
-import { createAction, Property } from "@activepieces/framework";
-import { operations as APIOperations, paths as APIPath } from './open-api-schema'
+import { createAction } from "@activepieces/framework";
+import {
+  operations as APIOperations,
+  paths as APIPaths,
+  components as APIComponents
+} from './open-api-schema'
 
-type CustomerPayment = APIOperations[keyof APIOperations]['responses'][keyof APIOperations[keyof APIOperations]['responses']]['content']['application/json']
+type Path = keyof APIPaths
+type Operation = keyof APIOperations
+type Schema = keyof APIComponents['schemas']
 
-export function openApiActions<T extends keyof APIPath>({
+export function openApiActions<
+  P extends Path,
+  O extends Operation,
+  S extends Schema
+>({
   path
 }: {
-  path: T
+  path: P
 }) {
   
-    
+
   return createAction({
     name: 'action_',
     displayName: "Action 1",
     description: "Simple",
     props: {
-      
+
     },
     sampleData: {},
     async run() {
