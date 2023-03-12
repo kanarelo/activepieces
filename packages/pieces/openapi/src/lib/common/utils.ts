020be4895b2bca7f130b8006a1e52a83c9232fc5
Mukewa O. Wekalao <onesmus.mukewa@gmail.com>
2023-03-13 01:29:55 +0300

    chore: adds openapi specifications

--- packages/pieces/openapi/src/lib/common/utils.ts
+++ packages/pieces/openapi/src/lib/common/utils.ts
@@ -1,5 +1,6 @@
 import { OpenAPI3 } from "openapi-typescript"
 import { dereference } from "@apidevtools/json-schema-ref-parser"
+import * as fs from 'fs'
 
 export const flattenReferences = (specification: OpenAPI3): OpenAPI3 => {
   let flattened: OpenAPI3 = specification
